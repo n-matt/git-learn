28a6e6137c4c8a310cbeb12415c504e86a7cfef3
n-matt <navjacinth91@gmail.com>
2023-11-28 13:40:50 +0530

    updated tests

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,33 +1,39 @@
+import unittest
+
 from src.calculator import Calculator
+import unittest
 
 
-def test_add():
-    assert Calculator.add(1, 2) == 3.0
-    assert Calculator.add(1.0, 2.0) == 3.0
-    assert Calculator.add(0, 2.0) == 2.0
-    assert Calculator.add(2.0, 0) == 2.0
-    assert Calculator.add(-4, 2.0) == -2.0
+class TestCalculator(unittest.TestCase):
 
+    def setUp(self):
+        self.cal = Calculator()
 
-def test_subtract():
-    assert Calculator.subtract(1, 2) == -1.0
-    assert Calculator.subtract(2, 1) == 1.0
-    assert Calculator.subtract(1.0, 2.0) == -1.0
-    assert Calculator.subtract(0, 2.0) == -2.0
-    assert Calculator.subtract(2.0, 0.0) == 2.0
-    assert Calculator.subtract(-4, 2.0) == -6.0
+    def test_add(self):
+        assert self.cal.add(1, 2) == 3.0
+        assert self.cal.add(1.0, 2.0) == 3.0
+        assert self.cal.add(0, 2.0) == 2.0
+        assert self.cal.add(2.0, 0) == 2.0
+        assert self.cal.add(-4, 2.0) == -2.0
 
+    def test_subtract(self):
+        assert self.cal.subtract(1, 2) == -1.0
+        assert self.cal.subtract(2, 1) == 1.0
+        assert self.cal.subtract(1.0, 2.0) == -1.0
+        assert self.cal.subtract(0, 2.0) == -2.0
+        assert self.cal.subtract(2.0, 0.0) == 2.0
+        assert self.cal.subtract(-4, 2.0) == -6.0
 
-def test_multiply():
-    assert Calculator.multiply(1, 2) == 2.0
-    assert Calculator.multiply(1.0, 2.0) == 2.0
-    assert Calculator.multiply(0, 2.0) == 0.0
-    assert Calculator.multiply(2.0, 0.0) == 0.0
-    assert Calculator.multiply(-4, 2.0) == -8.0
+    def test_multiply(self):
+        assert self.cal.multiply(1, 2) == 2.0
+        assert self.cal.multiply(1.0, 2.0) == 2.0
+        assert self.cal.multiply(0, 2.0) == 0.0
+        assert self.cal.multiply(2.0, 0.0) == 0.0
+        assert self.cal.multiply(-4, 2.0) == -8.0
 
-
-def test_divide():
-    assert Calculator.divide(1, 2) == 0.5
-    assert Calculator.divide(1.0, 2.0) == 0.5
-    assert Calculator.divide(0, 2.0) == 0
-    assert Calculator.divide(-4, 2.0) == -2.0
+    def test_divide(self):
+        assert self.cal.divide(1, 2) == 0.5
+        assert self.cal.divide(1.0, 2.0) == 0.5
+        assert self.cal.divide(0, 2.0) == 0
+        assert self.cal.divide(-4, 2.0) == -2.0
+        assert self.cal.divide(10, 0) == 'Cannot divide by 0'
