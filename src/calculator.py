3e80234383e8fd624336919b00009fd3c677f89f
n-matt <navjacinth91@gmail.com>
2023-11-28 14:08:19 +0530

    updated script

--- src/calculator.py
+++ src/calculator.py
@@ -15,3 +15,10 @@ class Calculator:
 
     def square(x, y):
         return x ** y
+
+    def get_split(val):
+        l1 = val.split("/")
+        name = l1[-1]
+        age = l1[-2]
+        pla = l1[-2]
+        return name, age, pla
