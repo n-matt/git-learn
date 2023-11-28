2b5be366ea834dcffdb8154e05f47887b6bcada2
n-matt <55640761+n-matt@users.noreply.github.com>
2023-11-28 14:01:13 +0530

    New file (#42)
    * added new script
    
    * updated file
    
    * updated tests
    
    * updated tests
    
    * updated tests
    
    * updated tests
    
    * updated script
    
    * updated script

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -31,3 +31,8 @@ def test_divide():
     assert Calculator.divide(1.0, 2.0) == 0.5
     assert Calculator.divide(0, 2.0) == 0
     assert Calculator.divide(-4, 2.0) == -2.0
+    assert Calculator.divide(10, 0) == 'Cannot divide by 0'
+
+
+def test_square():
+    assert Calculator.square(2, 2) == 4
