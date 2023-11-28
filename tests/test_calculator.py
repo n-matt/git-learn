7cfdcb353b4a7030ae69b811c1a74fead56356e8
n-matt <navjacinth91@gmail.com>
2023-11-28 13:49:28 +0530

    updated tests

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,39 +1,33 @@
-import unittest
-
 from src.calculator import Calculator
-import unittest
 
 
-class TestCalculator(unittest.TestCase):
+def test_add():
+    assert Calculator.add(1, 2) == 3.0
+    assert Calculator.add(1.0, 2.0) == 3.0
+    assert Calculator.add(0, 2.0) == 2.0
+    assert Calculator.add(2.0, 0) == 2.0
+    assert Calculator.add(-4, 2.0) == -2.0
 
-    def setUp(self):
-        self.cal = Calculator()
 
-    def test_add(self):
-        self.assertEquals(self.cal.add(1, 2), 3.0)
-        self.assertEquals(self.cal.add(1.0, 2.0), 3.0)
-        self.assertEquals(self.cal.add(0, 2.0), 2.0)
-        self.assertEquals(self.cal.add(2.0, 0), 2.0)
-        self.assertEquals(self.cal.add(-4, 2.0), -2.0)
+def test_subtract():
+    assert Calculator.subtract(1, 2) == -1.0
+    assert Calculator.subtract(2, 1) == 1.0
+    assert Calculator.subtract(1.0, 2.0) == -1.0
+    assert Calculator.subtract(0, 2.0) == -2.0
+    assert Calculator.subtract(2.0, 0.0) == 2.0
+    assert Calculator.subtract(-4, 2.0) == -6.0
 
-    def test_subtract(self):
-        self.assertEquals(self.cal.subtract(1, 2), -1.0)
-        self.assertEquals(self.cal.subtract(2, 1), 1.0)
-        self.assertEquals(self.cal.subtract(1.0, 2.0), -1.0)
-        self.assertEquals(self.cal.subtract(0, 2.0), -2.0)
-        self.assertEquals(self.cal.subtract(2.0, 0.0), 2.0)
-        self.assertEquals(self.cal.subtract(-4, 2.0), -6.0)
 
-    def test_multiply(self):
-        self.assertEquals(self.cal.multiply(1, 2), 2.0)
-        self.assertEquals(self.cal.multiply(1.0, 2.0), 2.0)
-        self.assertEquals(self.cal.multiply(0, 2.0), 0.0)
-        self.assertEquals(self.cal.multiply(2.0, 0.0), 0.0)
-        self.assertEquals(self.cal.multiply(-4, 2.0), -8.0)
+def test_multiply():
+    assert Calculator.multiply(1, 2) == 2.0
+    assert Calculator.multiply(1.0, 2.0) == 2.0
+    assert Calculator.multiply(0, 2.0) == 0.0
+    assert Calculator.multiply(2.0, 0.0) == 0.0
+    assert Calculator.multiply(-4, 2.0) == -8.0
 
-    def test_divide(self):
-        self.assertEquals(self.cal.divide(1, 2), 0.5)
-        self.assertEquals(self.cal.divide(1.0, 2.0), 0.5)
-        self.assertEquals(self.cal.divide(0, 2.0), 0)
-        self.assertEquals(self.cal.divide(-4, 2.0), -2.0)
-        self.assertEquals(self.cal.divide(10, 0), 'Cannot divide by 0')
+
+def test_divide():
+    assert Calculator.divide(1, 2) == 0.5
+    assert Calculator.divide(1.0, 2.0) == 0.5
+    assert Calculator.divide(0, 2.0) == 0
+    assert Calculator.divide(-4, 2.0) == -2.0
